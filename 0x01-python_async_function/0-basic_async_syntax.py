f2131faf57e1081ef3d64546f9ba4e7113a6cc1c
Justin4587 <Thurman@atlasok.com>
2022-02-01 20:51:30 -0600

    test

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -9,6 +9,6 @@ import random
 
 async def wait_random(max_delay: int = 10) -> float:
     """somethings going to happen bets on Traceback??"""
-    waitTime = max_delay * random.uniform(0, max_delay)
-    await asyncio.sleep(waitTime)
-    return waitTime
+    waited = random.uniform(0, max_delay)
+    await asyncio.sleep(waited)
+    return waited
