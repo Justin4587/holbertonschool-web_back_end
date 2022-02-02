c3bf976f6de8c97f6937804b14f53cf9a526d151
Justin4587 <Thurman@atlasok.com>
2022-02-02 00:11:14 -0600

    last time for 1

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -8,7 +8,7 @@ import time
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-async def measure_time(n: int, max_delay: int) -> float:
+def measure_time(n: int, max_delay: int) -> float:
     """somethings going to happen bets on Traceback??"""
     a = time.time()
 
@@ -16,4 +16,4 @@ async def measure_time(n: int, max_delay: int) -> float:
 
     b = time.time()
     
-    return (a - b) / n
+    return (b - a) / n
