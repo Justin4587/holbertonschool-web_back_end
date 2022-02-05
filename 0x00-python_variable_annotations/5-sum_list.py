82a381b1225d0bf03815915d21dfed387bdc8ce8
Justin4587 <Thurman@atlasok.com>
2022-02-05 14:18:06 -0600

    last minute

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 """
- Take a wild Guess a, pi, i
+    Take a wild Guess a, pi, i
 """
 
 from typing import List
 
 
 def sum_list(input_list: List[float]) -> float:
-  """somethings going to happen bets on Traceback?? """
-  return sum(input_list)
+    """somethings going to happen bets on Traceback?? """
+    return sum(input_list)
--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 """
- Take a wild Guess a, pi, i
+    Take a wild Guess a, pi, i
 """
 
 from typing import List, Union
 
 
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
-  """somethings going to happen bets on Traceback?? """
-  return sum(mxd_lst)
+    """somethings going to happen bets on Traceback?? """
+    return sum(mxd_lst)
