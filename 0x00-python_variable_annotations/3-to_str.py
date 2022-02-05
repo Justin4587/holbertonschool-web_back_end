a82fcdacadab121e771fe7effadbfa9954516857
Justin4587 <Thurman@atlasok.com>
2022-02-05 14:13:16 -0600

    last minute

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 """
- Take a wild Guess
+    Take a wild Guess
 """
 
 
 def to_str(n: float) -> str:
-  """
-    If any one ever reads this id love to know
-  """
-  return str(n)
+    """
+        If any one ever reads this id love to know
+    """
+    return str(n)
