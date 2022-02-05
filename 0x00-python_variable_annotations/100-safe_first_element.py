6a58857469e1eaa34ca1755677522d504bafe841
Justin4587 <Thurman@atlasok.com>
2022-02-05 15:00:45 -0600

    making it pretty

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 """
- Take a wild Guess a, pi, i
+    Take a wild Guess a, pi, i
 """
 
 from typing import Any, Sequence, Union
 
 
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
-  """somethings going to happen bets on Traceback?? """
-  if lst:
-    return lst[0]
-  else:
-    return None
+    """somethings going to happen bets on Traceback?? """
+    if lst:
+        return lst[0]
+    else:
+        return None
