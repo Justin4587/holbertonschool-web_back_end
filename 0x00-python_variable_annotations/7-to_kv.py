dc22079e0a6f1dfd1d3a044d9bebc359eaed40e8
Justin4587 <Thurman@atlasok.com>
2022-02-05 14:56:53 -0600

    making it pretty

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -6,6 +6,6 @@
 from typing import Union, Tuple
 
 
-def to_kv(k:str, v: Union[int, float]) -> Tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """somethings going to happen bets on Traceback?? """
     return (k, v * v)
