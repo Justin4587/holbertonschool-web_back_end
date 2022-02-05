74a82e2943942c68ed69b8b08db91f21bbbf135b
Justin4587 <Thurman@atlasok.com>
2022-02-04 19:01:25 -0600

    we are getting too serious about documenting

--- 0x03-caching/1-fifo_cache.py
+++ 0x03-caching/1-fifo_cache.py
@@ -2,18 +2,16 @@
 """
     let the caching commence
 """
-BaseCaching = __import__('base_caching').BaseCaching
+from base_caching import BaseCaching
 
 
-class BasicCache(BaseCaching):
+class FIFOCache(BaseCaching):
     """somethings going to happen bets on Traceback??"""
     def put(self, key, item):
-        """ Im placing some generic text here """
         if not key or not item:
             pass
         else:
             self.cache_data[key] = item
 
     def get(self, key):
-        """ bring me the keys"""
-        return self.cache_data.get(key)
+        return self.cache_data.get(key
