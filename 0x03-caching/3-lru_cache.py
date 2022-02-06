4180daf2b305297c5b7065150302e5c307087a99
Justin4587 <Thurman@atlasok.com>
2022-02-05 22:08:54 -0600

    I learned way more than I planned to about the change to dicts in python 3.6 and why they didnt make that change to sets

---
+++ 0x03-caching/3-lru_cache.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+    let the caching commence
+"""
+from base_caching import BaseCaching
+from collections import OrderedDict
+
+
+class LRUCache(BaseCaching):
+    """somethings going to happen bets on Traceback??"""
+    def __init__(self):
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """ Im placing some generic text here """
+
+        if key and item:
+            self.cache_data[key] = item
+            self.cache_data.move_to_end(key)
+
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            firstOut = self.cache_data.popitem(last=False)
+            print("DISCARD: " + str(firstOut[0]))
+
+    def get(self, key):
+        """ bring me the keys"""
+        if key in self.cache_data:
+            self.cache_data.move_to_end(key)
+            return self.cache_data[key]
