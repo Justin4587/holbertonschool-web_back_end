9c37712dd7ebdc661be06cb727755afc657dbed7
Justin4587 <Thurman@atlasok.com>
2022-02-10 21:10:19 -0600

    its learning

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -6,11 +6,11 @@ from typing import List
 
 
 def filter_datum(fields: List[str], redaction: str,
-                 message: str, seperator: str) -> str:
+                 message: str, separator: str) -> str:
     """ I believe I should be removing things """
 
     for _ in fields:
-        youveGotMail = re.sub(_ + ".*?" + seperator,
+        youveGotMail = re.sub(_ + ".*?" + separator,
                               _ + "=" + redaction + separator, message)
 
     return youveGotMail
