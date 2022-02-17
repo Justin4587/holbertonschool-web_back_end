6bb786c7f16576f874dff26f623317ab40a33552
Justin4587 <Thurman@atlasok.com>
2022-02-17 13:01:59 -0600

    task 6

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,11 +5,14 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """Basic Auth"""
-    
+
     def extract_base64_authorization_header(self,
                                             authorization_header: str) -> str:
         """ extract header """
-        if authorization_header is None or type(authorization_header) is not str:
+        if authorization_header is None:
+            return None
+
+        if type(authorization_header) is not str:
             return None
 
         if authorization_header.startswith("Basic "):
