7d3e735234926ae54a7cb1bccf77a7b0b5754b5b
Justin4587 <Thurman@atlasok.com>
2022-02-17 13:17:51 -0600

    task 8

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ Class Basic Auth """
 from api.v1.auth.auth import Auth
-
+from base64 import b64decode
 
 class BasicAuth(Auth):
     """Basic Auth"""
@@ -17,3 +17,18 @@ class BasicAuth(Auth):
 
         if authorization_header.startswith("Basic "):
             return authorization_header[6:]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str) -> str:
+        """ decode base64 header """
+        if base64_authorization_header is None:
+            return None
+
+        if type(base64_authorization_header) is not str:
+            return None
+
+        try:
+            return b64decode(base64_authorization_header,
+                             None, True).decode("utf-8")
+        except Exception:
+            return None
