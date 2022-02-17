1bda2b9aaae83b7f72544f7d014683608601beec
Justin4587 <Thurman@atlasok.com>
2022-02-17 11:01:41 -0600

    so far

---
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+Route module for the API
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth():
+    """ auth class """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ method to enforce authentication """
+        if path is None:
+            return True
+
+        if path[-1] != '/':
+            path += '/'
+
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """words that will equal points"""
+        if request is None:
+            return None
+        
+        return request.headers.get("Authorization")
+
+    def current_user(self, request=None) -> TypeVar("User"):
+        """ returns none """
+        return None
