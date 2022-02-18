940bbe7e21f877dca7ebaa5eb9d35dd9cad0ea9b
Justin4587 <Thurman@atlasok.com>
2022-02-17 18:55:04 -0600

    task 5

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -32,10 +32,12 @@ def before_request() -> str:
     """ before handler """
     paths = ['/api/v1/status/',
              '/api/v1/unauthorized/',
-             '/api/v1/forbidden/']
+             '/api/v1/forbidden/',
+             '/api/v1/auth_session/login/']
 
     if auth is not None and auth.require_auth(request.path, paths):
-        if auth.authorization_header(request) is None:
+        if auth.authorization_header(request) is None\
+            and auth.session_cookie(request) is None:
             abort(401)
         current_user = auth.current_user(request)
         if current_user is None:
