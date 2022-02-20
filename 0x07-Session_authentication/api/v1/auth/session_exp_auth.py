e6949459c79a2c834efa7f92e820fccebd9e5805
Justin4587 <Thurman@atlasok.com>
2022-02-20 15:47:57 -0600

    wont work local so lets see what happens

---
+++ 0x07-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+""" Session exp auth class def """
+
+from api.v1.auth.session_auth import SessionAuth
+from datetime import datetime, timedelta
+from os import getenv
+
+
+class SessionExpAuth(SessionAuth):
+    """ session exp """
+
+    def __init__(self):
+        """ overload innit """
+        if getenv("SESSION_DURATION"):
+            self.session_duration = int(getenv("SESSION_DURATION"))
+        else:
+            self.session_duration = 0
+
+    def create_session(self, user_id=None):
+        """creates something """
+        sessId = super().create_session(user_id) 
+        if sessId is None:
+            return None
+        self.user_id_by_session_id[sessId] = {
+            "user_id": user_id,
+            "created_at": datetime.now()}
+        return sessId
+
+    def user_id_for_session_id(self, session_id=None):
+        """ id for id """
+        if session_id is None:
+            return None
+        sess_dict = self.user_id_by_session_id.get(session_id)
+        if sess_dict is None:
+            return None
+        if self.session_duration <= 0:
+            return sess_dict.get("user_id")
+        if "created_at" not in sess_dict:
+            return None
+        sess_time = sess_dict.get("created_at") + timedelta(seconds=self.session_duration)
+        if sess_time < datetime.now():
+            return None
+        return sess_dict.get("user_id")
