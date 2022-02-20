50985b9ae35c66df6a1b043ab425ba7ad21398ea
Justin4587 <Thurman@atlasok.com>
2022-02-20 17:38:17 -0600

    I think Ive got all but the wierd 500 code BS i cant figure out

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -37,6 +37,7 @@ if getenv("AUTH_TYPE") == "session_db_auth":
     from api.v1.auth.session_db_auth import SessionDBAuth
     auth = SessionDBAuth()
 
+
 @app.before_request
 def before_request() -> str:
     """ before handler """
@@ -50,7 +51,7 @@ def before_request() -> str:
         if auth.authorization_header(request) is None\
                 and auth.session_cookie(request) is None:
             abort(401)
-        
+
         if auth.current_user(request) is None:
             abort(403)
         request.current_user = auth.current_user(request)
