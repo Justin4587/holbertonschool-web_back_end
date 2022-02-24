eb2a0d250d4f85daee9b64c0a74e5d3ddfc5c374
Justin4587 <Thurman@atlasok.com>
2022-02-23 21:52:29 -0600

    curious to see how the checker is handling the None issue here

---
+++ 0x08-user_authentication_service/0x08new new newnew newer/user.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" user class SQLAlchemy mapping declaration """
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+Base = declarative_base()
+
+
+class User(Base):
+    """ User class from base """
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250))
+    reset_token = Column(String(250))
