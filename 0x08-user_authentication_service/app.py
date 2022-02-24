8a15e19d66c3a171b8672d0c24c7e61717e84739
Justin4587 <Thurman@atlasok.com>
2022-02-23 21:59:09 -0600

    curious to see how the checker is handling the None issue here

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 from crypt import methods
-from flask import Flask, jsonify, request, abort
+from flask import Flask, jsonify, request
 from auth import Auth
-
 app = Flask(__name__)
-auth = Auth()
+AUTH = Auth()
 
 
 @app.route("/", methods=["GET"])
@@ -17,29 +16,10 @@ def reg_user() -> str:
     try:
         email = request.form.get("email")
         password = request.form.get("password")
-        auth.register_user(email, password)
+        AUTH.register_user(email, password)
         return jsonify({"email": email, "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"})
-
-@app.route("/sessions", methods=["POST"], strict_slashes=False)
-def login() -> str:
-    try:
-        email = request.form.get("email")
-    except:
-        return
-    try:
-        password = request.form.get("password")
-    except:
-        return
-    if auth.valid_login(email, password) is True:
-        s_id = auth.create_session(email)
-        resp = jsonify({"email": email,
-        "message": "logged in"})
-        resp.set_cookie("session_id", s_id)
-        return resp
-    else:
-        abort(401)
 
 
 if __name__ == "__main__":
