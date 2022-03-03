b9dcfefec3fb79721388b4e15ebec6d73426efea
Justin4587 <Thurman@atlasok.com>
2022-03-02 23:38:54 -0600

    parameterized parameterized parameterized

---
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""All the tests from everywhere have gathered here"""
+from parameterized import parameterized
+import unittest
+from utils import access_nested_map
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """im just picturing a group of old school maps huddled together"""
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
+
+    def test_access_nested_map(self, nested_map, path, expected):
+        """checks nested map return"""
+        self.assertEqual(access_nested_map(nested_map, path), expected)
