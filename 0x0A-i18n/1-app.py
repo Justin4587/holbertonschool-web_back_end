872147fea225a32e1cc175ab9b27480aa91737ba
Justin4587 <Thurman@atlasok.com>
2022-03-03 21:57:51 -0600

    The tower of babel story was when I called BS on that whole damn thing

--- 0x0A-i18n/1-app.py
+++ 0x0A-i18n/1-app.py
@@ -8,10 +8,10 @@ babel = Babel(app)
 
 
 class Config(object):
-    """ config the tower of babel """
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
+	""" config the tower of babel """
+	LANGUAGES = ["en", "fr"]
+	BABEL_DEFAULT_LOCALE = "en"
+	BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 app.config.from_object(Config)
@@ -19,9 +19,9 @@ app.config.from_object(Config)
 
 @app.route("/")
 def index():
-    """ render html page """
-    return render_template("1-index.html")
+	""" render html page """
+	return render_template("1-index.html")
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+	app.run(host="0.0.0.0", port="5000")
