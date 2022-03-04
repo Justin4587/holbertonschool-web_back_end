20c02c25e0b2bffb99c192dd1194acaef386b0b9
Justin4587 <Thurman@atlasok.com>
2022-03-03 22:29:25 -0600

    easy points?

---
+++ 0x0A-i18n/2-app.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+""" flask module """
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config(object):
+    """ config the tower of babel """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """ localization best match selector """
+    return render_template("2-index.html")
+
+
+@app.route("/")
+def index():
+    """ render html page """
+    return render_template("1-index.html")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
