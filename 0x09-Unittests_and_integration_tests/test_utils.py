254dc24309633d125df4fae1de02bb87866c2b1c
Justin4587 <Thurman@atlasok.com>
2022-03-04 23:07:02 -0600

    Not a fan of testing

--- 0x09-Unittests_and_integration_tests/test_utils.py
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -22,5 +22,22 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": 1}, ("a", "b"))
     ])
     def test_access_nested_map_exception(self, nested_map, path):
+        """checks exception status"""
         with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """testing function ability to get json"""
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+
+    def test_get_json(self, test_url, test_payload):
+        """its in the def"""
+        with patch("requests.get") as mock:
+            mock().json.return_value = test_payload
+            mock.assert_called_once()
+            response = get_json(test_url)
+            self.assertEqual(response, test_payload)
