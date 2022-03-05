768dd7b3ad49168a7215680ff55f4498d8f70fbb
Justin4587 <Thurman@atlasok.com>
2022-03-04 23:25:48 -0600

    Not a fan of testing

--- 0x09-Unittests_and_integration_tests/test_utils.py
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -2,7 +2,7 @@
 """All the tests from everywhere have gathered here"""
 from parameterized import parameterized
 import unittest
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 from unittest.mock import patch
 
 
@@ -42,3 +42,26 @@ class TestGetJson(unittest.TestCase):
             mock.assert_called_once()
             response = get_json(test_url)
             self.assertEqual(response, test_payload)
+
+
+class TestMemoize(unittest.TestCase):
+    """ I wonder what it will do """
+    def test_memoize(self):
+        """ hmm copy and paste """
+        
+        class TestClass:
+            """ more remarks about stuff"""
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(TestClass, "a_method", return_value=42) as mock:
+            test = TestClass()
+            one = test.a_property
+            two = test.a_property
+            self.assertEqual(one, 42)
+            self.assertEqual(two, 42)
+            mock.assert_called_once()
