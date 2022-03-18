72ed831e8ca62a71744e114f2e2fed9548e804cd
Justin Thurman <1828@holbertonschool.com>
2022-03-17 23:42:54 -0500

    Not sure what check I'm missing and because of that I'm going to call it a night, Hopefully tomorrow I can figure out how to actually test it local

--- 0x0C-MySQL_Advanced/5-valid_email.sql
+++ 0x0C-MySQL_Advanced/5-valid_email.sql
@@ -1,12 +1,10 @@
 -- Create trigger to reset valid email
 -- Is this the problem
-DELIMETER $$
+DELIMETER |
 CREATE TRIGGER email_reset BEFORE UPDATE ON users
 FOR EACH ROW
 BEGIN
     IF OLD.email <> NEW.email THEN
     SET NEW.valid_email = 0;
     END IF;
-END$$
-
-DELIMETER ;
+END;
