275c3df8d87d954df8acc4ae641c7631af4521fb
Justin Thurman <1828@holbertonschool.com>
2022-03-19 19:37:27 -0500

    raise 0

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """something about redis"""
 import redis
 from uuid import uuid4
-from typing import Union
+from typing import Union, Callable
 
 
 class Cache():
@@ -18,3 +18,19 @@ class Cache():
         key = str(uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Callable = None) -> Union[str, bytes, int,
+                                                          float]:
+        """somethings going to happen bets on Traceback??"""
+        if fn:
+            return fn(self._redis.get(key))
+        else:
+            return self._redis.get(key)
+
+    def get_str(self, key: str) -> str:
+        """something about redis"""
+        return self.get(key, str)
+
+    def get_int(self, key: str) -> int:
+        """something about redis"""
+        return self.get(key, int)
