e80a57d314a7acdf836714a2a8d806c2c541a636
Justin Thurman <1828@holbertonschool.com>
2022-03-20 01:27:07 -0500

    I'm not sure what the fuck I'm doing but this might get some points with no alarms

--- 0x0B_redis_basic/web.py
+++ 0x0B_redis_basic/web.py
@@ -10,19 +10,13 @@ _redis = redis.Redis()
 
 def get_page_count(method: Callable) -> Callable:
     """something about redis"""
-    print("hello")
 
     @wraps(method)
-    def wrapper(url):
-        print(f"this is from args {url}")
-        key = _redis.incr(f"count:{url}")
-        cached = _redis.get(f"cached:{url}")
-        if cached:
-            return cached.decode('utf-8')
-        else:
-            _redis.setex(f"cached:{url}", 10, key)
-            
-            return
+    def wrapper(*args):
+        key = f"count:{args[0]}"
+        _redis.incr(key)
+        _redis.setex('count', 10, _redis.get(key))
+        return method(*args)
 
     return wrapper
 
@@ -30,8 +24,5 @@ def get_page_count(method: Callable) -> Callable:
 @get_page_count
 def get_page(url: str) -> str:
     """something about redis"""
-    print("hello before .get(url)")
     req = requests.get(url)
-    print("hello after .get(url)")
-    print(f"this the req.text {req.text}")
     return req.text
