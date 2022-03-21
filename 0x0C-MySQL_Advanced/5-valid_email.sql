05187309a4db592220bcba59a00d69ac977e0a78
Justin Thurman <1828@holbertonschool.com>
2022-03-20 20:53:36 -0500

    This Deli Meter is a pain in the ass

--- 0x0C-MySQL_Advanced/5-valid_email.sql
+++ 0x0C-MySQL_Advanced/5-valid_email.sql
@@ -1,9 +1,11 @@
 -- Create trigger to reset valid email
 -- Is this the problem
+DELIMETER $$
 CREATE TRIGGER email_reset BEFORE UPDATE ON users
 FOR EACH ROW
 BEGIN
     IF OLD.email <> NEW.email THEN
     SET NEW.valid_email = 0;
     END IF;
-END;
+END$$
+DELIMETER;
