640ac070469b2bdf3ed79f0b43b0be0becd0e7e3
Justin Thurman <1828@holbertonschool.com>
2022-03-22 13:03:14 -0500

    find

---
+++ 0x0D-NoSQL/101-students.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""list all or empty"""
+
+
+def top_students(mongo_collection):
+    """ the stuff"""
+    return mongo_collection.find({'topics': topic})
+
+if __name__ == "__main__":
+    top_students(mongo_collection)
