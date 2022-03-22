b57e39a2632cda6b2063ea4d38a6e5bfc57ca763
Justin Thurman <1828@holbertonschool.com>
2022-03-22 11:49:11 -0500

    Another try but same

--- 0x0D-NoSQL/9-insert_school.py
+++ 0x0D-NoSQL/9-insert_school.py
@@ -4,7 +4,11 @@
 
 def insert_school(mongo_collection, **kwargs):
     """ the stuff"""
-    return mongo_collection.insert_one(kwargs)
+    coll = {}
+    for k, v in kwargs.items():
+        coll[k] = v
+    docId = mongo_collection.insert_one(coll)
+    return docId
 
 if __name__ == "__main__":
     insert_school(mongo_collection, **kwargs)
