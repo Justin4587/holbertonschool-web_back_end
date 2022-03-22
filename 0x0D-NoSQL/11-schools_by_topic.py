b89afb6224ff23f66ae14e1f759fe1b02c613a26
Justin Thurman <1828@holbertonschool.com>
2022-03-22 12:16:37 -0500

    lets see

--- 0x0D-NoSQL/11-schools_by_topic.py
+++ 0x0D-NoSQL/11-schools_by_topic.py
@@ -4,7 +4,7 @@
 
 def schools_by_topic(mongo_collection, topic):
     """ the stuff"""
-    mongo_collection.find({'topics': topic})
+    return mongo_collection.find({'topics': topic})
 
 if __name__ == "__main__":
     schools_by_topic(mongo_collection, topics)
