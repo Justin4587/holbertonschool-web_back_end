5f4b2a43de5d669a3619423fb666c229ddd05ccf
Justin Thurman <1828@holbertonschool.com>
2022-04-03 13:54:38 -0500

    fucking curlys

--- 0x0F-ES6_promise/3-all.js
+++ 0x0F-ES6_promise/3-all.js
@@ -3,7 +3,11 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   return Promise.all([
     uploadPhoto(),
-    createUser]).then((txt) => {
-    console.log(txt[0].body, createUser());
+    createUser()]).then((txt) => {
+    const txt0 = txt[0].body;
+    const txt1 = txt[1].firstName;
+    const txt2 = txt[1].lastName;
+    const message = txt0 + ' ' + txt1 + ' ' + txt2;
+    console.log(message);
   }).catch(() => console.log('Signup system offline'));
 }
