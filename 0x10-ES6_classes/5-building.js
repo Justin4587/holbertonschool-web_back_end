ee8908df549416c117e6561d6d15514cb88cd8dd
Justin Thurman <1828@holbertonschool.com>
2022-04-02 22:32:08 -0500

    method

--- 0x10-ES6_classes/5-building.js
+++ 0x10-ES6_classes/5-building.js
@@ -1,15 +1,10 @@
-/* eslint-disable */
 export default class Building {
   constructor(sqft) {
     this._sqft = sqft;
 
-    if (!this.evacuationWarningMessage) {
+    if (this.constructor !== Building && !this.evacuationWarningMessage) {
       throw Error('Class extending Building must overide evacuationWarningMessage');
-    };
-  }
-
-  evacuationWarningMessage() {
-    return;
+    }
   }
 
   get sqft() {
