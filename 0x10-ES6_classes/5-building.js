a074bf437ff75bcc84e8474f228023ad82e5117e
Justin Thurman <1828@holbertonschool.com>
2022-04-02 23:13:33 -0500

    should I be using reflection or putting the method in Building this shit sucks

--- 0x10-ES6_classes/5-building.js
+++ 0x10-ES6_classes/5-building.js
@@ -3,7 +3,7 @@ export default class Building {
     this._sqft = sqft;
 
     if (this.constructor !== Building && !this.evacuationWarningMessage) {
-      throw Error('Class extending Building must overide evacuationWarningMessage');
+      throw Error('Class extending Building must override evacuationWarningMessage');
     }
   }
 
