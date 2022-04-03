f276ca1f07b6082e308aca3ab3113d1dc0cacd19
Justin Thurman <1828@holbertonschool.com>
2022-04-02 21:23:10 -0500

    curly braces suck

---
+++ 0x10-ES6_classes/4-pricing.js
@@ -0,0 +1,34 @@
+import Currency from './3-currency.js';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  set amount(aamount) {
+    this._amount = aamount;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set currency(acurrency) {
+    this._currency = acurrency;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  displayFullPrice() {
+    // const lp = ' (';
+    // const rp = ')';
+    const amo = this._amount;
+    const spce = ' ';
+    // const cur = this._currency;
+    const fuck = this.currency.displayFullCurrency();
+    return amo + spce + fuck;
+  }
+}
