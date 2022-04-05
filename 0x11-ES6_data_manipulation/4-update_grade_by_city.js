fab9c371a0e9fe940dcc5d866b9d2cbb8aa542c3
Justin Thurman <1828@holbertonschool.com>
2022-04-05 15:26:04 -0500

    that was a bitch

--- 0x11-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x11-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,12 +1,12 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
-  let wierd = 'N/A';
+  let grade = 'N/A';
   const filtStu = students.filter((student) => student.location === city);
   const newerGrades = filtStu.map((student) => {
     const newGrade = newGrades.filter((thing) => thing.studentId === student.id);
     if (newGrade[0]) {
-      wierd = newGrade[0].grade;
+      grade = newGrade[0].grade;
     }
-    return { ...student, wierd };
+    return { ...student, grade };
   });
   return newerGrades;
 }
